7569481b692592b4b86fbc0f90259e6d4e925de3
CJAlvarez <cristhianalvarez96@gmail.com>
2019-08-13 12:05:11 -0600

    patients buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 
 // Components
@@ -9,6 +11,12 @@ import { NotFoundComponent } from './ui/pages/not-found/not-found.component';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './ui/components/navbar/navbar.component';
 import { PatientsComponent } from './ui/pages/patients/patients.component';
+import { PatientManageButtonsComponent } from './ui/components/patient-manage-buttons/patient-manage-buttons.component';
+import { ListPatientsComponent } from './ui/components/list-patients/list-patients.component';
+import { CreatePatientsComponent } from './ui/components/create-patients/create-patients.component';
+
+// Services
+import { PatientsService } from './api/patients/patients.service';
 
 // Routing
 const routes: Routes = [
@@ -17,20 +25,26 @@ const routes: Routes = [
   { path: '**', component: NotFoundComponent }
 ];
 
-
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
     NotFoundComponent,
     NavbarComponent,
-    PatientsComponent
+    PatientsComponent,
+    PatientManageButtonsComponent,
+    ListPatientsComponent,
+    CreatePatientsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    PatientsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/ui/components/patient-manage-buttons/patient-manage-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-manage-buttons',
+  templateUrl: './patient-manage-buttons.component.html',
+  styleUrls: ['./patient-manage-buttons.component.css']
+})
+export class PatientManageButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
